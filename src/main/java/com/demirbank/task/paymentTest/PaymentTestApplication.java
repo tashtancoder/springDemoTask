2e5b255fd1d91c1bbb296831afab00d06e2f71b9
Tashtan <satievtashtan@gmail.com>
2022-10-25 08:17:05 +0300

    rollback

---
+++ src/main/java/com/demirbank/task/paymentTest/PaymentTestApplication.java
@@ -0,0 +1,13 @@
+package com.demirbank.task.paymentTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentTestApplication.class, args);
+	}
+
+}
