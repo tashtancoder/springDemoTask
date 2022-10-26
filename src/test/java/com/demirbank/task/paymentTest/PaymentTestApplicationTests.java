e4ceaa1af428e7ab73593bd340e6c454914c35c3
Tashtan <satievtashtan@gmail.com>
2022-10-26 14:07:57 +0300

    main skeleton of the task is created

---
+++ src/test/java/com/demirbank/task/paymentTest/PaymentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demirbank.task.paymentTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
